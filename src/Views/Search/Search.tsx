cd1b9c995529efe6ef427f44a48eb69dd63401ea
Joy Kim <wlasl0121@gmail.com>
2022-08-31 19:31:22 +0900

    modify 최근 검색 기록

--- src/Views/Search/Search.tsx
+++ src/Views/Search/Search.tsx
@@ -8,13 +8,34 @@ export default function Search() {
     const [lat, setLat] = useState(0);
     const [lon, setLon] = useState(0);
 
+    const [searchList, setSearchList] = useState<any>(
+        JSON.parse(localStorage.getItem("searchList") || "[]")
+    );
+
     const enter = () => {
         if (address) {
             window.open(PATHS.searchStr + address, "_self");
+            addSearch(address)
+                .then((r) => {
+                    console.log(r);
+                    localStorage.setItem("searchList", JSON.stringify(r));
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
         } else {
             console.log(address);
         }
     };
+
+    async function addSearch(address: string) {
+        const newAddr = {
+            value: address,
+        };
+        const arr = [newAddr, ...searchList];
+        setSearchList([newAddr, ...searchList]);
+        return arr;
+    }
 
     function currentLocation() {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -41,7 +62,24 @@ export default function Search() {
             <div className={style.list_container}>
                 <ChargerContainer lat={lat} lon={lon} />
                 <div className={style.record_title}>{"최근검색"}</div>
-                {/* 검색기록 리스트 */}
+                <div className={style.search_list_container}>
+                    {searchList ? (
+                        searchList.map((item: any) => {
+                            return (
+                                <div className={style.search_list_item}>
+                                    <div className={style.icon} />
+                                    <div className={style.text}>
+                                        {item.value}
+                                    </div>
+                                    <div className={style.empty} />
+                                    <div className={style.del_icon} />
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <div />
+                    )}
+                </div>
             </div>
         </div>
     );
